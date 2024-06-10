3f02192739e071f9befeebd394d79d6a188a603a
YinQingYuanQue <dawnmoon@unm.edu>
2024-06-10 15:16:35 -0600

    Fuzzy logic created

---
+++ Code/Fuz_Memb.m
@@ -0,0 +1,20 @@
+% set the full path
+fis = readfis('../Fuzzy/Fuzzy_rules/Fuzzy_3fac_939.fis');
+
+raw_data = readmatrix('../Fuzzy/Fuzzy_table/Input_939.csv');
+data = raw_data(:,2:4);
+
+FM = evalfis(fis,data)
+
+FM(:,4) = raw_data(:,1)
+writematrix(FM,'../Fuzzy/Fuzzy_table/Output_939.csv')
+
+%hold all
+%plotmf(fis,'input',2)
+%plot(82.52,1,'squre')
+%plot(82.52,0,'squre')
+%plot(477.6,1,'squre')
+%plot(477.6,0,'squre')
+%xline(82.52,'--')
+%xline(477.6,'--')
+%xticks([82.52 200 300 400 477.6 600])
